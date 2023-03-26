efe965b7e42f15d59a8de4a0b23ae395fbecb74f
MarkusFletcher <cehatopfatalist@gmail.com>
2023-03-26 19:12:11 +0300

    add placeholder

--- src/js/classes/select.js
+++ src/js/classes/select.js
@@ -1,4 +1,5 @@
-const getTemplate = (options = []) => {
+const getTemplate = (options = [], placeholder) => {
+	placeholder = placeholder ?? options[0].text
 	const $options = options.map((el) => {
 		return `<li class="select__option" data-type="option" data-id="${el.id}" data-value=${el.value}>${el.text}</li>`
 	})
@@ -6,7 +7,7 @@ const getTemplate = (options = []) => {
 	return `
 	<div class="select__backdrop" data-type="backdrop"></div>
 	<div class="select__input" data-type="input">
-		<span data-type="value">${options[0].text}</span>
+		<span data-type="value">${placeholder}</span>
 		<svg class="select__arrow" width="9" height="6" viewBox="0 0 9 6" fill="none" xmlns="http://www.w3.org/2000/svg">
 			<path fill="#fff" d="M8.93375 1.51079C8.93375 1.43936 8.89803 1.359 8.84446 1.30543L8.39803 0.859C8.34446 0.805429 8.2641 0.769714 8.19268 0.769714C8.12125 0.769714 8.04089 0.805429 7.98732 0.859L4.47839 4.36793L0.969462 0.859C0.91589 0.805429 0.835533 0.769714 0.764105 0.769714C0.683748 0.769714 0.612319 0.805429 0.558748 0.859L0.112319 1.30543C0.0587475 1.359 0.0230331 1.43936 0.0230331 1.51079C0.0230331 1.58221 0.0587475 1.66257 0.112319 1.71614L4.27303 5.87686C4.3266 5.93043 4.40696 5.96614 4.47839 5.96614C4.54982 5.96614 4.63018 5.93043 4.68375 5.87686L8.84446 1.71614C8.89803 1.66257 8.93375 1.58221 8.93375 1.51079Z"/>
 		</svg>
@@ -19,7 +20,7 @@ const getTemplate = (options = []) => {
 }
 
 export class Select {
-	constructor(selector) {
+	constructor(selector, placeholder) {
 		
 		this.$select = document.querySelector(selector)
 		this.$options = this.$select.querySelectorAll('option')
@@ -29,8 +30,8 @@ export class Select {
 				value: el.value,
 				text: el.innerText
 			}
-		}) 
-		
+		})
+		this.placeholder = placeholder
 		this.selectedId = null
 
 		this.render()
@@ -39,15 +40,16 @@ export class Select {
 
 	render() {
 		this.$element = document.createElement('div')
+		this.moveClasses()
 		this.$select.parentNode.insertBefore(this.$element, this.$select);
 		this.$select.classList.add('select__select')
 		this.$element.appendChild(this.$select);
 		this.$element.classList.add('select')
-
-		this.$element.insertAdjacentHTML('beforeEnd', getTemplate(this.options))
+		this.$element.insertAdjacentHTML('beforeEnd', getTemplate(this.options, this.placeholder))
 	}
 
 	setup() {
+		if(this.placeholder) this.$select.value = ''
 		this.clickHandler = this.clickHandler.bind(this)
 		this.$element.addEventListener('click', this.clickHandler)
 		this.$value = this.$element.querySelector('[data-type="value"]')
@@ -79,6 +81,14 @@ export class Select {
 		this.$value.textContent = this.current.text
 		this.$select.value = this.current.value
 		this.close()
+	}
+
+	moveClasses() {
+		const classList =  [...this.$select.classList]
+		classList.forEach(className => {
+			this.$select.classList.remove(className)
+			this.$element.classList.add(className)
+		})
 	}
 
 	toggle() {
